6b652a400d5401651f7c247c93d5e2cc20ef5b3e
kurubas <emrekurubas@gmail.com>
2018-03-11 11:36:51 +0200

    Basic functionality implemented.

--- app.js
+++ app.js
@@ -1,7 +1,75 @@
 const http = require('http');
 const router = require('./router.js');
+var nconf = require('nconf');
+var Twit = require('twit');
 
 const port = 3001;
+
+var locations = {
+    finland: '21,59,29,67',
+    us: '-125,33,-66,44',
+    germany: '8,48,13,54',
+    turkey: '27,36,43,41',
+    france: '-4,44,7,49',
+    india: '68,8,86,26',
+    china: '100,23,124,43',
+    russia: '34,53,177,70',
+    australia: '110,-38,153,-12',
+    southafrica: '16,-35,31,-23',
+    brazil: '-54,-27,-32,4'
+};
+
+// -------------- WEBSOCKET ---------------------
+
+var ws = require("nodejs-websocket")
+var tweetStream = {}
+var connected = false;
+
+// Scream server example: "hi" -> "HI!!!"
+const websocketServer = ws.createServer(function (connection) {
+	console.log('New connection')
+	connection.on('text', function (location) {
+		console.log('Location: ' + location);
+        connected = true;
+        nconf.file({ file: 'config.json' }).env();
+
+        var twitter = new Twit({
+          consumer_key: nconf.get('TWITTER_CONSUMER_KEY'),
+          consumer_secret: nconf.get('TWITTER_CONSUMER_SECRET'),
+          access_token: nconf.get('TWITTER_ACCESS_TOKEN'),
+          access_token_secret: nconf.get('TWITTER_ACCESS_TOKEN_SECRET')
+        });
+        
+        tweetStream = twitter.stream('statuses/filter', { locations: locations[location] });
+        tweetStream.on('tweet', function (tweet) {
+            if (tweet) {
+                var date = new Date(
+                    tweet.created_at.replace(/^\w+ (\w+) (\d+) ([\d:]+) \+0000 (\d+)$/,
+                    "$1 $2 $4 $3 UTC"));
+                // TODO : Find a better format for date.
+                var text = '[' + date.toLocaleString() + '] ' + tweet.user.name + ": " + tweet.text;
+                console.log(tweet);
+                if (connected) {
+                    connection.sendText(text);        
+                }
+            }
+        });
+	})
+    
+	connection.on('close', function () {
+        connected = false;
+        console.log('Connection closed');
+        
+        if (tweetStream !== null) {  
+            tweetStream.stop();
+            tweetStream = null;
+        }
+	})
+});
+
+websocketServer.listen(8081);
+
+// -------------- WEB SERVER ---------------------
 
 const server = http.createServer((request, response) => {
     router.home(request, response);
---
+++ client.js
@@ -0,0 +1,101 @@
+var connection = {};
+
+$(".finland").on("click", function(e) { handleClick(e) });
+$(".us").on("click", function(e) { handleClick(e) });
+$(".germany").on("click", function(e) { handleClick(e) });
+$(".turkey").on("click", function(e) { handleClick(e) });
+$(".france").on("click", function(e) { handleClick(e) });
+$(".india").on("click", function(e) { handleClick(e) });
+$(".china").on("click", function(e) { handleClick(e) });
+$(".russia").on("click", function(e) { handleClick(e) });
+$(".southafrica").on("click", function(e) { handleClick(e) });
+$(".australia").on("click", function(e) { handleClick(e) });
+$(".brazil").on("click", function(e) { handleClick(e) });
+
+var currentLocation = {};
+
+// Handle popup opening, use websockets to retrieve live data from webserver.
+function handleClick(e){
+    e.preventDefault();
+    document.getElementById('myModal').style.display = "block";
+    
+    resetPopup();
+    
+    var location = e.currentTarget.className;
+    initializeWebSocket(location);
+}
+
+function initializeWebSocket(location) {
+    connection = new WebSocket("ws://" + window.location.hostname + ":8081")
+    connection.onopen = function (e) { connection.send(location); }; 
+    currentLocation = location;
+    
+    connection.onclose = function(e) {
+        if (connection.readyState == 3) {
+            var message = "Connection lost.";
+            console.error(message);
+            setPopupHeader(message);    
+        }
+    }
+    
+    connection.onerror = function () {
+        var message = "An error occured.";
+        console.error(message);
+        setPopupHeader(message);
+    }
+    
+    connection.onmessage = function (event) {
+        var modal = document.getElementsByClassName('modal-internal')[0];
+        var div = document.createElement("p")
+        div.textContent = event.data
+        modal.appendChild(div)
+        modal.scrollTop = modal.scrollHeight;
+    }
+    
+    setPopupHeader('Loading new tweets from ' + location.toUpperCase() + '. Please wait...');
+}
+
+function setPopupHeader(text) {
+    var modalContent = document.getElementsByClassName('modal-content')[0];
+    var p = modalContent.getElementsByClassName('inner-header')[0];
+    p.textContent = text; 
+}
+
+$(".play-button").on("click", function(e){
+    var isPlaying = e.currentTarget.textContent == "Pause";
+    if (isPlaying) {
+        connection.close();
+        e.currentTarget.textContent = "Play";
+        setPopupHeader('Connection to ' + currentLocation.toUpperCase() + ' has paused.');
+    }
+    else {
+        e.currentTarget.textContent = "Pause";
+        initializeWebSocket(currentLocation);
+    }
+});
+
+
+function resetPopup() {
+    playButton = document.getElementsByClassName('play-button')[0];
+    playButton.textContent = "Pause";
+    
+    var modal = document.getElementsByClassName('modal-internal')[0];
+    while (modal.firstChild) {
+        modal.removeChild(modal.firstChild);
+    }
+}
+
+// Handle popup closing when pressing x
+$(".close").on("click", function(e){
+    document.getElementById('myModal').style.display = "none";
+    connection.close();
+});
+
+// Handle popup closing when clicking somewhere outside the popup.
+window.onclick = function(event) {
+    var modal = document.getElementById('myModal');
+    if (event.target == modal) {
+        modal.style.display = "none";
+        connection.close();
+    }
+}
